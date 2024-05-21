93576b91c596467a5dd543fc54db9d5c92768bd3
dargamenteria <dargamenteria@paranoidworld.es>
2024-05-21 07:02:42 +0200

    Added our test application

---
+++ test/rest/api_test.py
@@ -0,0 +1,59 @@
+import http.client
+import os
+import unittest
+from urllib.request import urlopen
+
+import pytest
+
+BASE_URL = "http://localhost:5000"
+BASE_URL_MOCK = "http://localhost:9090"
+DEFAULT_TIMEOUT = 20  # in secs
+
+
+@pytest.mark.api
+class TestApi(unittest.TestCase):
+    def setUp(self):
+        self.assertIsNotNone(BASE_URL, "URL no configurada")
+        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/4/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(response.read().decode(), "2.0", "ERROR DIVIDE")
+
+    def test_api_divide_by_zero(self):
+        with pytest.raises(Exception) as e:
+            url = f"{BASE_URL}/calc/divide/4/0"
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        assert str(e.value) == "HTTP Error 406: NOT ACCEPTABLE"
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(response.read().decode(), "2", "ERROR MULTIPLY")
+
+    def test_api_add(self):
+        url = f"{BASE_URL}/calc/add/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(response.read().decode(), "3", "ERROR ADD")
+
+    def test_api_sqrt(self):
+        url = f"{BASE_URL_MOCK}/calc/sqrt/64"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(response.read().decode(), "8", "ERROR SQRT")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
